aa5757663e5e6b483c3a1933d2b070f495a54061
JHJP <78130703+JHJP@users.noreply.github.com>
2022-05-21 01:27:48 +0900

    create crawled data from investing.com with stock history tabe data

--- src/WebScrape.java
+++ src/WebScrape.java
@@ -1,37 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class WebScrape {
     public static void main(String[] args) {
+    	List<String> date = new ArrayList<String>();
+    	List<String> price = new ArrayList<String>();
+    	List<String> open = new ArrayList<String>();
+    	List<String> high = new ArrayList<String>();
+    	List<String> low = new ArrayList<String>();
+    	List<String> vol = new ArrayList<String>();
+    	List<String> change = new ArrayList<String>();
         final String url = 
-                "https://www.investing.com/";
+                "https://au.investing.com/equities/apple-computer-inc-historical-data";
         
         try {
             final Document document = Jsoup.connect(url).get();
-            
-            for (Element row : document.select(
-                "table.tablesorter.full tr")) {
-                if (row.select("td:nth-of-type(1)").text().equals("")) {
-                    continue;
-                }
-                else {
-                   final String ticker = 
-                           row.select("td:nth-of-type(1)").text();
-                   final String name = 
-                           row.select("td:nth-of-type(2)").text();
-                   final String tempPrice = 
-                           row.select("td.right:nth-of-type(3)").text();
-                   final String tempPrice1 = 
-                           tempPrice.replace(",", "");
-//                    final double price = Double.parseDouble(tempPrice1);
-                   
-                    System.out.println(ticker + " " + name + " " + tempPrice1);
-                }
+            Elements table = document.select("table");
+            Element tBody = table.get(1); //select the first table.
+            Elements rows = tBody.select("tr");
+            for (int i = 1; i < rows.size(); i++) { //first row is the col names so skip it.
+                Element row = rows.get(i);
+                Elements cols = row.select("td");
+                date.add(cols.get(0).text());
+                price.add(cols.get(1).text());
+                open.add(cols.get(2).text());
+                high.add(cols.get(3).text());
+                low.add(cols.get(4).text());
+                vol.add(cols.get(5).text());
+                change.add(cols.get(6).text());
             }
+            System.out.println(date);
+            System.out.println(price);
+            System.out.println(open);
+            System.out.println(high);
+            System.out.println(low);
+            System.out.println(vol);
+            System.out.println(change);
+            
         }
-        catch (Exception ex) {
-            ex.printStackTrace();
+        catch (Exception e) {
+            e.printStackTrace();
         }
         
     }
